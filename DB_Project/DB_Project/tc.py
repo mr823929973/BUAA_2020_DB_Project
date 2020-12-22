8e7c2b6e94bca47edf9f05aecc1787e222e43c14
Caesar41 <365074151@qq.com>
2020-12-23 00:11:37 +0800

    add manage.py

--- DB_Project/DB_Project/tc.py
+++ DB_Project/DB_Project/tc.py
@@ -38,46 +38,6 @@ def getTinfo(t, ctx):
     ctx['user'] = t
 
 
-def select(request):
-    ctx = {}
-    if request.POST:
-        s = request.POST['user']
-        t = request.POST['tc']
-        t = TC.objects.filter(pk=t).first()
-        s = Student.objects.filter(Sno=s).first()
-        sc = SC.objects.filter(Sno=s.Sno, Cno=t.Cno)
-
-        al = SC.objects.filter(TC=t).count()
-
-        if len(sc) != 0:
-            ctx['m1'] = '该学生已选择过同名的课，不能重复选课！'
-        elif t.Dno != s.Dno:
-            ctx['m1'] = '只能选择本系开设的课程！'
-        elif t.Cno.v <= al:
-            ctx['m1'] = '人数已满，选课失败！'
-        else:
-            SC.objects.create(Sno=s, Cno=t.Cno, TC=t)
-            ctx['m1'] = '选课成功！'
-        getSinfo(s, ctx)
-    return render(request, "top.html", ctx)
-
-
-def delete(request):
-    ctx = {}
-    if request.POST:
-        s = request.POST['user']
-        c = request.POST['course']
-        s = Student.objects.filter(Sno=s).first()
-        sc = SC.objects.filter(Sno=s.Sno, Cno=c)
-        if len(sc) == 0:
-            ctx['m1'] = '该学生没有选择该课，无法退课！'
-        else:
-            sc.delete()
-            ctx['m1'] = '退课成功！'
-        getSinfo(s, ctx)
-    return render(request, "top.html", ctx)
-
-
 def add(request):
     ctx = {}
     if request.POST:
